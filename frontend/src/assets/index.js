bd4c42db9527f511d55a31d6c0d7e90ecbc6d40d
VietNT1810 <vietnt@beedu.vn>
2022-08-23 17:21:08 +0700

    assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,32 @@
+import logoWhite from '@/assets/images/logo-white.png';
+import homeImage from '@/assets/images/statistic-img.jpg';
+import homePoster01 from '@/assets/images/poster-01.png';
+import homePoster02 from '@/assets/images/poster-02.jpg';
+import homePoster03 from '@/assets/images/poster-03.jpg';
+import homePoster04 from '@/assets/images/poster-04.jpg';
+import homeAvatar01 from '@/assets/images/ava/001.png';
+import homeAvatar02 from '@/assets/images/ava/002.png';
+import homeAvatar03 from '@/assets/images/ava/003.png';
+import iconFacebook from '@/assets/icons/facebook-icon.svg';
+import iconTwitter from '@/assets/icons/twitter-icon.svg';
+import iconYoutube from '@/assets/icons/youtube-icon.svg';
+
+const images = {
+    logoWhite,
+    homeImage,
+    homePoster01,
+    homePoster02,
+    homePoster03,
+    homePoster04,
+    homeAvatar01,
+    homeAvatar02,
+    homeAvatar03,
+};
+
+const icons = {
+    iconFacebook,
+    iconTwitter,
+    iconYoutube,
+};
+
+export default { images, icons };
