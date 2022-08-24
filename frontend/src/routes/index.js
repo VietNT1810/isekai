46a43e2e6871a39b1ad47cb30663561ef3d78474
VietNT1810 <vietnt@beedu.vn>
2022-08-24 18:02:24 +0700

    router

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,15 +1,19 @@
 import DefaultLayout from '@/layouts/DefaultLayout';
 import SpecialLayout from '@/layouts/SpecialLayout';
-import ShopLayout from '@/layouts/ShopLayout';
+import ShopLayout from '@/layouts/ShopLayout/ShopLayout';
+import AuthLayout from '@/layouts/AuthLayout';
 
 import Special from '@/pages/Special';
 import Home from '@/pages/Home';
 import Shop from '@/pages/Shop';
+import Login from '@/pages/Login';
 
 const publicRoute = [
     { path: '/', component: Home, layout: DefaultLayout },
     { path: '/shop', component: Shop, layout: ShopLayout },
     { path: '/special', component: Special, layout: SpecialLayout },
+    { path: '/login', component: Login, layout: AuthLayout },
+    { path: '/register', component: Special, layout: AuthLayout },
 ];
 
-export { publicRoute }
+export { publicRoute };
