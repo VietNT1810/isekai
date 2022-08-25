d803ee1081cdf4da0dc62461bcebb0cbe9b0969c
VietNT1810 <vietnt@beedu.vn>
2022-08-25 17:59:58 +0700

    login form component

--- frontend/src/pages/Login/components/LoginForm/LoginForm.jsx
+++ frontend/src/pages/Login/components/LoginForm/LoginForm.jsx
@@ -4,10 +4,18 @@ import classNames from 'classnames/bind';
 import styles from './LoginForm.module.scss';
 import InputField from '@/components/InputField';
 import { NavLink } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import { Checkbox, FormControlLabel, Typography } from '@mui/material';
 
 const cx = classNames.bind(styles);
 
 function LoginForm(props) {
+    const { register, control, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+
     return (
         <div className={cx('login-form')}>
             <h1>Đăng nhập</h1>
@@ -15,7 +23,45 @@ function LoginForm(props) {
                 Người mới?
                 <NavLink to="/register">Tạo tài khoản ở đây</NavLink>
             </h4>
-            <InputField variant="filled" label="Email" fullWidth />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <InputField variant="filled" id="email" label="Email" name="email" fullWidth register={register} />
+                <InputField
+                    variant="filled"
+                    id="password"
+                    type="password"
+                    label="Password"
+                    name="password"
+                    fullWidth
+                    register={register}
+                    autoComplete="on"
+                />
+                <small>
+                    <NavLink to="/register">Quên mật khẩu ?</NavLink>
+                </small>
+                <Controller
+                    name="remember"
+                    control={control}
+                    rules={{ required: false }}
+                    render={({ field }) => (
+                        <FormControlLabel
+                            label={<Typography sx={{ fontSize: 16, fontWeight: "bold" }}>Giữ tôi luôn đăng nhập</Typography>}
+                            control={
+                                <Checkbox
+                                    {...field}
+                                    sx={{
+                                        '& .MuiSvgIcon-root': { fontSize: 28 },
+                                        color: '#ebf9fb',
+                                        '&.Mui-checked': {
+                                            color: '#ebf9fb',
+                                        },
+                                    }}
+                                />
+                            }
+                        />
+                    )}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
