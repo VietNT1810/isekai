fc0cbf59bddc5e3373233ea2e417c47b94b14fc9
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-08-31 11:27:13 +0700

    redux toolkit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,17 @@ import { render } from 'react-dom';
 
 import App from './App';
 import GlobalStyles from '@/components/GlobalStyles';
+import store from '@/redux/store';
+import { Provider } from 'react-redux';
 
 const rootElement = document.getElementById('root');
 render(
     <React.StrictMode>
-        <GlobalStyles>
-            <App />
-        </GlobalStyles>
+        <Provider store={store}>
+            <GlobalStyles>
+                <App />
+            </GlobalStyles>
+        </Provider>
     </React.StrictMode>,
     rootElement,
 );
