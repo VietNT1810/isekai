ed6dda22b069f2603f26d5e2da3d42d09a625d69
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-08-31 10:44:08 +0700

    update assets

--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -7,11 +7,6 @@ import homePoster04 from '@/assets/images/poster-04.jpg';
 import homeAvatar01 from '@/assets/images/ava/001.png';
 import homeAvatar02 from '@/assets/images/ava/002.png';
 import homeAvatar03 from '@/assets/images/ava/003.png';
-import iconFacebook from '@/assets/icons/facebook-icon.svg';
-import iconTwitter from '@/assets/icons/twitter-icon.svg';
-import iconYoutube from '@/assets/icons/youtube-icon.svg';
-import iconLinkedin from '@/assets/icons/linkedin-icon.svg';
-import iconGoogle from '@/assets/icons/google-icon.svg';
 import loginImage from '@/assets/images/bg-login.png';
 import registerImage from '@/assets/images/img-register.png';
 import specialBanner from '@/assets/images/banner/banner-special.png';
@@ -19,7 +14,19 @@ import specialCard01 from '@/assets/images/card-1.png';
 import specialCard02 from '@/assets/images/card-2.png';
 import specialCard03 from '@/assets/images/card-3.png';
 import specialCard04 from '@/assets/images/card-4.png';
-import specialCard05 from '@/assets/images/card-5.png'; 
+import specialCard05 from '@/assets/images/card-5.png';
+import shopBanner from '@/assets/images/banner/shop.png';
+import weaponBanner from '@/assets/images/banner/weapon.png';
+import wigBanner from '@/assets/images/banner/wig.png';
+import outfitBanner from '@/assets/images/banner/outfit.png';
+import lolitaBanner from '@/assets/images/banner/lolita.png';
+import accessoryBanner from '@/assets/images/banner/accessory.png';
+
+import iconFacebook from '@/assets/icons/facebook-icon.svg';
+import iconTwitter from '@/assets/icons/twitter-icon.svg';
+import iconYoutube from '@/assets/icons/youtube-icon.svg';
+import iconLinkedin from '@/assets/icons/linkedin-icon.svg';
+import iconGoogle from '@/assets/icons/google-icon.svg';
 
 const images = {
     logoWhite,
@@ -39,6 +46,12 @@ const images = {
     specialCard03,
     specialCard04,
     specialCard05,
+    shopBanner,
+    weaponBanner,
+    wigBanner,
+    outfitBanner,
+    lolitaBanner,
+    accessoryBanner,
 };
 
 const icons = {
