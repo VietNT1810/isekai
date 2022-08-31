1d004c6859fb3dc8961b7cd6b05f36bbf571b369
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-08-31 10:44:18 +0700

    shop layout

--- frontend/src/layouts/ShopLayout/ShopLayout.jsx
+++ frontend/src/layouts/ShopLayout/ShopLayout.jsx
@@ -1,18 +1,35 @@
+import assets from '@/assets';
+import { useLocation } from 'react-router-dom';
+
 import FilterSidebar from '../components/FilterSidebar';
 import Header from '../components/Header';
 
 function ShopLayout({ children, banner }) {
+    const location = useLocation();
+    const currentPath = location.pathname;
+
+    const getBanner = () => {
+        const bannerByRoute = {
+            '/shop': assets.images.shopBanner,
+            '/weapon': assets.images.weaponBanner,
+            '/wig': assets.images.wigBanner,
+            '/outfit': assets.images.outfitBanner,
+            '/lolita': assets.images.lolitaBanner,
+            '/accessory': assets.images.accessoryBanner,
+        };
+        return bannerByRoute[currentPath] || '';
+    };
+
     return (
         <>
+            {console.log(currentPath)}
             <Header />
             <div className="banner">
-                <img src={banner} alt="Error image" />
+                <img src={getBanner()} alt="Error image" />
             </div>
             <div className="container">
                 <FilterSidebar />
-                <div className="content">
-                    {children}
-                </div>
+                <div className="content">{children}</div>
             </div>
         </>
     );
