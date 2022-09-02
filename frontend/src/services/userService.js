8d8d6128e369cf5e2f4a5131df65f524ceef22e0
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-09-02 10:40:28 +0700

    user service

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,21 @@
+import request from '@/utils/request';
+
+const prefix = '/api/users';
+
+export const register = async (params, config) => {
+    try {
+        const result = await request.post(`${prefix}/signup`, params, config);
+        return result.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const login = async (params, config) => {
+    try {
+        const result = await request.post(`${prefix}/login`, params, config);
+        return result.data;
+    } catch (error) {
+        throw error;
+    }
+};
