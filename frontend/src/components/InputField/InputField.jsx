c8fed1576efc138156b6b9fc2c7a8a2531f00bdb
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-09-11 12:58:26 +0700

    input field

--- frontend/src/components/InputField/InputField.jsx
+++ frontend/src/components/InputField/InputField.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames/bind';
+import { FilledInput, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 import styles from './InputField.module.scss';
-import { FilledInput, FormControl, FormHelperText, InputLabel } from '@mui/material';
 
 const cx = classNames.bind(styles);
 
@@ -19,29 +20,49 @@ function InputField({
     id,
     ...passProps
 }) {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
     return (
-        <FormControl
-            fullWidth={fullWidth}
-            variant={variant}
-            error={error}
-            margin="dense"
-            sx={{
-                backgroundColor: '#ffffff',
-                borderRadius: '5px',
-                border: '2px solid #1e69a2'
-            }}
-        >
-            <InputLabel htmlFor={id} sx={{ fontSize: 16 }}>
+        <FormControl fullWidth={fullWidth} variant={variant} error={error} margin="dense" className={cx('input-group')}>
+            <InputLabel htmlFor={id} className={cx('label')}>
                 {label}
             </InputLabel>
             <FilledInput
                 disableUnderline
                 id={id}
                 name={name}
-                type={type || 'text'}
-                sx={{ fontSize: 16 }}
+                type={showPassword ? 'text' : type}
+                className={cx('input')}
                 {...register(name, { required: required })}
                 {...passProps}
+                endAdornment={
+                    type === 'password' ? (
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                                size="large"
+                                className={cx('input')}
+                            >
+                                {showPassword ? (
+                                    <VisibilityOff fontSize="inherit" />
+                                ) : (
+                                    <Visibility fontSize="inherit" />
+                                )}
+                            </IconButton>
+                        </InputAdornment>
+                    ) : null
+                }
             />
             {helperText && <FormHelperText id="component-error-text">{helperText}</FormHelperText>}
         </FormControl>
