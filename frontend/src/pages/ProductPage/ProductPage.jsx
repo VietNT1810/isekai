fb351e52540afe8d19c0c2d17675616a980c31a5
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-09-27 15:30:37 +0700

    product page

--- frontend/src/pages/ProductPage/ProductPage.jsx
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -2,34 +2,50 @@ import { Rating } from '@mui/material';
 import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Add, AddShoppingCart, Remove } from '@mui/icons-material';
 
 import assets from '@/assets';
 import Button from '@/components/Button';
 import { formatVND } from '@/helpers/number';
 import * as productsService from '@/services/productsService';
-import { Add, AddShoppingCart, Remove } from '@mui/icons-material';
+import * as reviewsService from '@/services/reviewsService';
 import styles from './ProductPage.module.scss';
+import { formatTime } from '@/helpers/string';
 
 const cx = classNames.bind(styles);
 
 function ProductPage(props) {
     const params = useParams();
     const [product, setProducts] = useState({});
+    const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
         const fetchProduct = async () => {
             await productsService
                 .getProduct(params.slug)
                 .then((res) => {
-                    setProducts(res.data.content);
+                    setProducts(res.data.content[0]);
                 })
                 .catch((err) => {
                     console.log(err);
                 });
         };
+
+        const fetchReviews = async () => {
+            await reviewsService
+                .getReviews(params.slug)
+                .then((res) => {
+                    console.log('review:', res.data.content);
+                    setReviews(res.data.content);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        };
+
+        fetchReviews();
         fetchProduct();
     }, []);
-    console.log(product);
 
     return (
         <div className={cx('wrapper')}>
@@ -47,7 +63,7 @@ function ProductPage(props) {
                             <Rating
                                 className={cx('rating')}
                                 name="read-only"
-                                defaultValue={product.rating || 0}
+                                value={product.averageRating || 0}
                                 size="large"
                                 sx={{
                                     '& .MuiRating-iconFilled': {
@@ -100,6 +116,53 @@ function ProductPage(props) {
                             <Button primary>Mua ngay</Button>
                         </div>
                     </div>
+                </div>
+                <div className={cx('review')}>
+                    <div className={cx('title')}>NHẬN XÉT TỪ KHÁCH HÀNG</div>
+                    <div className={cx('rating-summary')}>
+                        <span className={cx('rating-point')}>{product.averageRating}</span>
+                        <div className={cx('rating-star')}>
+                            <Rating
+                                className={cx('rating-star_icon')}
+                                value={product.averageRating || 0}
+                                size="large"
+                                sx={{
+                                    '& .MuiRating-iconFilled': {
+                                        color: '#04c4d9',
+                                    },
+                                }}
+                                readOnly
+                            />
+                            <span className={cx('rating-total')}>{product.totalReviews} nhận xét</span>
+                        </div>
+                    </div>
+                    {reviews.map((review) => (
+                        <div className={cx('review-bar')} key={review._id}>
+                            <div className={cx('user')}>
+                                <div className={cx('user-avatar')}>
+                                    <img src={review.user.avatar} alt="" />
+                                </div>
+                                <div className={cx('user-info')}>
+                                    <div className={cx('name')}>{review.user.username}</div>
+                                    <div className={cx('time-create')}>{formatTime(review.createdAt)}</div>
+                                </div>
+                            </div>
+                            <div className={cx('review-info')}>
+                                <Rating
+                                    className={cx('rating')}
+                                    value={review.rating || 0}
+                                    size="large"
+                                    sx={{
+                                        '& .MuiRating-iconFilled': {
+                                            color: '#04c4d9',
+                                        },
+                                    }}
+                                    readOnly
+                                />
+                                <div className={cx('comment')}>{review.review}</div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
---
+++ frontend/src/services/reviewsService.js
@@ -0,0 +1,10 @@
+import request from '@/utils/request';
+
+export const getReviews = async (slug) => {
+    try {
+        const result = await request.get(`/api/reviews/${slug}`);
+        return result.data;
+    } catch (error) {
+        return error;
+    }
+};
