a13f9ebc3235e09d88aad5d7c0adab6f1af3a410
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-09-28 17:07:23 +0700

    avatar preview

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,18 +1,40 @@
-import Button from '@/components/Button';
-import { hidePhone } from '@/helpers/number';
-import { hideEmail } from '@/helpers/string';
 import { Divider } from '@mui/material';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
+import Button from '@/components/Button';
+import { hidePhone } from '@/helpers/number';
+import { hideEmail } from '@/helpers/string';
+
+import assets from '@/assets';
 import styles from './Profile.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Profile(props) {
     const { loading, userInfo } = useSelector((state) => state.user);
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewAvatar, setPreviewAvatar] = useState('');
+
     let phone = '0868402367';
+    //handle file
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+    };
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewAvatar(reader.result);
+        };
+    };
+
+    // console.log('userInfo:', userInfo);
+    // console.log('previewAvatar:', previewAvatar);
 
     return (
         <div className={cx('wrapper')}>
@@ -24,12 +46,22 @@ function Profile(props) {
                 <div className={cx('content')}>
                     <div className={cx('avatar')}>
                         <label htmlFor="uploadAvatar" className={cx('upload-image')}>
-                            <img src="https://picsum.photos/200" alt="" />
+                            <img
+                                src={previewAvatar || userInfo?.avatar || assets.icons.iconCamera}
+                                alt="Avatar error"
+                            />
                         </label>
                         <label htmlFor="uploadAvatar" className={cx('upload-btn')}>
                             Chọn ảnh
                         </label>
-                        <input type="file" id="uploadAvatar" accept=".jpg,.jpeg,.png" hidden />
+                        <input
+                            type="file"
+                            id="uploadAvatar"
+                            accept=".jpg,.jpeg,.png"
+                            value={fileInputState}
+                            onChange={handleFileInputChange}
+                            hidden
+                        />
                         <div className={cx('file-accept')}>
                             <small>Dụng lượng file tối đa 1 MB</small>
                             <small>Định dạng: .JPEG, .PNG</small>
@@ -40,7 +72,7 @@ function Profile(props) {
                         <form className={cx('form')}>
                             <div className={cx('form-group')}>
                                 <label htmlFor="username">Username:</label>
-                                <span>Admin1</span>
+                                <span>admin</span>
                             </div>
                             <div className={cx('form-group')}>
                                 <label htmlFor="name">Tên:</label>
