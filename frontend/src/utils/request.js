fd0a1bb5002c936f6f978d0c0cc5d8c91296c92d
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-09-30 12:08:31 +0700

    request utils

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -2,10 +2,13 @@ import axios from 'axios';
 
 const API_URL = import.meta.env.VITE_BASE_URL;
 
+const userToken = localStorage.getItem('access-token') ? localStorage.getItem('access-token') : null;
+
 const request = axios.create({
     baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${userToken}`,
     },
 });
 
