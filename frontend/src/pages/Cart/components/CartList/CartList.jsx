7c200371107c9963ec07dd7ca438fc6ff8f43067
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-10-10 12:08:24 +0700

    delete dialog

--- frontend/src/pages/Cart/components/CartList/CartList.jsx
+++ frontend/src/pages/Cart/components/CartList/CartList.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import { Add, DeleteOutline, Remove } from '@mui/icons-material';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import classNames from 'classnames/bind';
+import { useState } from 'react';
 
 import styles from './CartList.module.scss';
 import { formatVND } from '@/helpers/number';
-import { Add, DeleteOutline, Remove } from '@mui/icons-material';
+import Button from '@/components/Button';
 
 const cx = classNames.bind(styles);
 
 function CartList({ carts }) {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDeleteConfirm = () => {
+        setOpen(false);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleQuantityChange = (e) => {
+        console.log(e.target.value);
+    };
+
     return (
         <div className={cx('cart-list')}>
             {carts.map((cart) => (
@@ -33,13 +53,7 @@ function CartList({ carts }) {
                             <button>
                                 <Remove />
                             </button>
-                            <input
-                                type="text"
-                                value={cart.quantity}
-                                onChange={() => {
-                                    console.log();
-                                }}
-                            />
+                            <input type="text" value={cart.quantity} onChange={handleQuantityChange} />
                             <button>
                                 <Add />
                             </button>
@@ -47,10 +61,34 @@ function CartList({ carts }) {
                     </div>
                     <span className={cx('final-price')}>{formatVND(cart.productId.price * cart.quantity)}</span>
                     <div className={cx('delete-btn')}>
-                        <DeleteOutline fontSize="large" />
+                        <DeleteOutline fontSize="large" onClick={handleClickOpen} />
                     </div>
                 </div>
             ))}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                className={cx('dialog')}
+            >
+                <DialogTitle id="alert-dialog-title" className={cx('dialog-title')}>
+                    Xóa sản phẩm ?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description" className={cx('dialog-content')}>
+                        Bạn có muốn xóa sản phẩm đang chọn?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button outline className={cx('dialog-button')} onClick={handleDeleteConfirm}>
+                        Xác nhận
+                    </Button>
+                    <Button primary className={cx('dialog-button')} onClick={handleClose}>
+                        Hủy
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
