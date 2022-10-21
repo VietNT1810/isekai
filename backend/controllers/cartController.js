32f20f6f75289b2ce0368c19d1f737d4dd80c502
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-10-21 15:39:48 +0700

    cart controller

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -109,6 +109,9 @@ const getCart = async (req, res) => {
         select: ["name", "price", "productImage", "slug"],
       },
     });
+    if (!cart) {
+      return res.status(404).json({ error: "No such cart here!" });
+    }
     res.status(200).json({ cart });
   } catch (error) {
     res.status(400).json({ error: error.message });
