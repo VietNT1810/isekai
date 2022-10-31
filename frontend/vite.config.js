a1387693ea98eae36f3d45b3621c58e9a46e78ea
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-10-31 10:43:26 +0700

    rewrite to allow dot in params

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react(), svgr(), pluginRewriteAll()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
-})
+});
