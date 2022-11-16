187e9f07968d3bde11c8a2865f4377098e2d7c24
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-11-16 14:32:36 +0700

    header responsive

--- frontend/src/layouts/components/Header/Header.jsx
+++ frontend/src/layouts/components/Header/Header.jsx
@@ -1,5 +1,6 @@
-import { ShoppingCartTwoTone } from '@mui/icons-material';
-import { Badge, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { MenuRounded, ShoppingCartTwoTone } from '@mui/icons-material';
+import { Avatar, Badge, IconButton, useMediaQuery } from '@mui/material';
 import classNames from 'classnames/bind';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
@@ -13,6 +14,8 @@ import { initCart } from '@/pages/Cart/cartSlice';
 const cx = classNames.bind(styles);
 
 function Header() {
+    const matches = useMediaQuery('(max-width: 768px)');
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
     const { loading, userInfo } = useSelector((state) => state.user);
     const { carts } = useSelector((state) => state.cart);
     const navigate = useNavigate();
@@ -34,22 +37,43 @@ function Header() {
         navigate('/cart');
     };
 
+    console.log('matches:', matches);
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('container')}>
+                <div className={cx('hamburger')}>
+                    <IconButton
+                        edge="start"
+                        onClick={() => {
+                            setIsNavExpanded(!isNavExpanded);
+                        }}
+                    >
+                        <MenuRounded className={cx('hamburger-icon')} />
+                    </IconButton>
+                </div>
                 <div className={cx('brand')}>
                     <NavLink to="/">
                         <img src={assets.images.logoWhite} alt="Error image" />
                     </NavLink>
                 </div>
-                <div className={cx('nav')}>
-                    <NavLink className={({ isActive }) => (isActive ? cx('activeNav') : '')} to="/">
+                <div className={cx('nav', { expanded: isNavExpanded })}>
+                    <NavLink
+                        className={({ isActive }) => (isActive ? cx('activeNav', 'nav-item') : cx('nav-item'))}
+                        to="/"
+                    >
                         Trang chủ
                     </NavLink>
-                    <NavLink className={({ isActive }) => (isActive ? cx('activeNav') : '')} to="/shop">
+                    <NavLink
+                        className={({ isActive }) => (isActive ? cx('activeNav', 'nav-item') : cx('nav-item'))}
+                        to="/shop"
+                    >
                         Cửa hàng
                     </NavLink>
-                    <NavLink className={({ isActive }) => (isActive ? cx('activeNav') : '')} to="/special">
+                    <NavLink
+                        className={({ isActive }) => (isActive ? cx('activeNav', 'nav-item') : cx('nav-item'))}
+                        to="/special"
+                    >
                         Đặc biệt
                     </NavLink>
                 </div>
@@ -71,8 +95,22 @@ function Header() {
                     </div>
                 ) : (
                     <div className={cx('actions')}>
-                        <Link to="/login">Đăng nhập</Link>
-                        <Link to="/register">Đăng ký</Link>
+                        {matches ? (
+                            <IconButton
+                                aria-label="delete"
+                                size="large"
+                                onClick={() => {
+                                    navigate('/login');
+                                }}
+                            >
+                                <Avatar src="/broken-image.jpg" />
+                            </IconButton>
+                        ) : (
+                            <div className={cx('actions')}>
+                                <Link to="/login">Đăng nhập</Link>
+                                <Link to="/register">Đăng ký</Link>
+                            </div>
+                        )}
                     </div>
                 )}
             </div>
