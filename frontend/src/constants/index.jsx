b3fbd191f1af3a90050ad99e47345e5562b78b96
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2022-12-12 11:11:55 +0700

    profile nav constant

--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -1,6 +1,11 @@
+import { Star } from '@mui/icons-material';
+import PersonIcon from '@mui/icons-material/Person';
+import ChromeReaderModeIcon from '@mui/icons-material/ChromeReaderMode';
+import PlaceIcon from '@mui/icons-material/Place';
+import KeyIcon from '@mui/icons-material/Key';
+
 import TextIcon from '@/components/TextIcon';
 import { formatVND } from '@/helpers/number';
-import { Star } from '@mui/icons-material';
 
 export const PRICE_MARKS = [
     { value: 100000, label: formatVND(100000) },
@@ -14,4 +19,11 @@ export const RATING_MARKS = [
     { value: 3, label: <TextIcon text="3" icon={<Star />} /> },
     { value: 4, label: <TextIcon text="4" icon={<Star />} /> },
     { value: 5, label: <TextIcon text="5" icon={<Star />} /> },
+];
+
+export const PROFILE_NAV = [
+    { to: 'profile', icon: <PersonIcon fontSize="large" color="action" />, title: 'Thông tin tài khoản' },
+    { to: 'order', icon: <ChromeReaderModeIcon fontSize="large" color="action" />, title: 'Quản lý đơn hàng' },
+    { to: 'address', icon: <PlaceIcon fontSize="large" color="action" />, title: 'Thêm địa chỉ' },
+    { to: 'change-password', icon: <KeyIcon fontSize="large" color="action" />, title: 'Thay đổi mật khẩu' },
 ];
