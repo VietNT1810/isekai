3f130164165e3fed47789d45a7ed96ac1ded25c3
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-02-12 10:31:12 +0700

    add product description

--- frontend/src/pages/ProductPage/components/ProductDetail/ProductDetail.jsx
+++ frontend/src/pages/ProductPage/components/ProductDetail/ProductDetail.jsx
@@ -23,6 +23,7 @@ function ProductDetail({ product, isInCart }) {
     const [quantity, setQuantity] = useState(1);
     const [openAlert, setOpenAlert] = useState(false);
     const [message, setMessage] = useState('');
+    const [text, setText] = useState('');
 
     const handleQuantityChange = (e) => {
         const regex = /^[0-9\b]+$/;
@@ -89,88 +90,109 @@ function ProductDetail({ product, isInCart }) {
 
     return (
         <div className={cx('product')}>
-            <div className={cx('product-image-container')}>
-                <div className={cx('product-image-box')}>
-                    <img src={product.productImage} className={cx('product-image')} alt="Error image" />
-                </div>
-            </div>
-            <div className={cx('product-info')}>
-                <span className={cx('product-name')}>{product.name}</span>
-                <div className={cx('product-group')}>
-                    <p className={cx('label')}>Đánh giá:</p>
-                    <Rating
-                        className={cx('rating')}
-                        name="read-only"
-                        value={product.averageRating || 0}
-                        size="large"
-                        sx={{
-                            '& .MuiRating-iconFilled': {
-                                color: '#04c4d9',
-                            },
-                        }}
-                        readOnly
-                    />
-                </div>
-
-                <div className={cx('product-group')}>
-                    <p className={cx('label')}>Giá:</p>
-                    <span className={cx('price')}>{formatVND(product.price)}</span>
-                </div>
-
-                <div className={cx('product-group', 'product-quantity')}>
-                    <p className={cx('label')}>Số lượng:</p>
-                    <div className={cx('quantity')}>
-                        <div className={cx('quantity-box')}>
-                            <div className={cx('cart-quantity')}>
-                                <button onClick={handleDecreaseQuantity} disabled={quantity == 1}>
-                                    <Remove />
-                                </button>
-                                <input
-                                    type="tel"
-                                    value={quantity}
-                                    onChange={handleQuantityChange}
-                                    onBlur={handleQuantityBlur}
-                                />
-                                <button onClick={handleIncreaseQuantity}>
-                                    <Add />
-                                </button>
-                            </div>
-                            <span>{product.quantity} sản phẩm có sẵn</span>
-                        </div>
-                        {quantity > product.quantity ? (
-                            <div className={cx('quantity-error')}>
-                                <span>Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này</span>
-                            </div>
-                        ) : null}
+            <div className={cx('product-detail')}>
+                <div className={cx('product-image-container')}>
+                    <div className={cx('product-image-box')}>
+                        <img src={product.productImage} className={cx('product-image')} alt="Error image" />
                     </div>
                 </div>
+                <div className={cx('product-info')}>
+                    <span className={cx('product-name')}>{product.name}</span>
+                    <div className={cx('product-group')}>
+                        <p className={cx('label')}>Đánh giá:</p>
+                        <Rating
+                            className={cx('rating')}
+                            name="read-only"
+                            value={product.averageRating || 0}
+                            size="large"
+                            sx={{
+                                '& .MuiRating-iconFilled': {
+                                    color: '#04c4d9',
+                                },
+                            }}
+                            readOnly
+                        />
+                    </div>
 
-                <div className={cx('product-group')}>
-                    <p className={cx('label')}>Vận chuyển:</p>
-                    <h1 className={cx('shipping')}>
-                        <img src={assets.icons.iconShipping} alt="Error icon" />
-                        <span>Miễn phí vận chuyển</span>
-                    </h1>
+                    <div className={cx('product-group')}>
+                        <p className={cx('label')}>Giá:</p>
+                        <span className={cx('price')}>{formatVND(product.price)}</span>
+                    </div>
+
+                    <div className={cx('product-group', 'product-quantity')}>
+                        <p className={cx('label')}>Số lượng:</p>
+                        <div className={cx('quantity')}>
+                            <div className={cx('quantity-box')}>
+                                <div className={cx('cart-quantity')}>
+                                    <button onClick={handleDecreaseQuantity} disabled={quantity == 1}>
+                                        <Remove />
+                                    </button>
+                                    <input
+                                        type="tel"
+                                        value={quantity}
+                                        onChange={handleQuantityChange}
+                                        onBlur={handleQuantityBlur}
+                                    />
+                                    <button onClick={handleIncreaseQuantity}>
+                                        <Add />
+                                    </button>
+                                </div>
+                                <span>{product.quantity} sản phẩm có sẵn</span>
+                            </div>
+                            {quantity > product.quantity ? (
+                                <div className={cx('quantity-error')}>
+                                    <span>Số lượng bạn chọn đã đạt mức tối đa của sản phẩm này</span>
+                                </div>
+                            ) : null}
+                        </div>
+                    </div>
+
+                    <div className={cx('product-group')}>
+                        <p className={cx('label')}>Vận chuyển:</p>
+                        <h1 className={cx('shipping')}>
+                            <img src={assets.icons.iconShipping} alt="Error icon" />
+                            <span>Miễn phí vận chuyển</span>
+                        </h1>
+                    </div>
+
+                    <div className={cx('product-action')}>
+                        {isInCart ? (
+                            <Button
+                                outline
+                                leftIcon={<RemoveShoppingCartOutlined color="primary" sx={{ fontSize: '24px' }} />}
+                                onClick={handleRemoveCart}
+                            >
+                                Xóa khỏi giỏ hàng
+                            </Button>
+                        ) : (
+                            <Button
+                                outline
+                                leftIcon={<AddShoppingCartOutlined color="primary" sx={{ fontSize: '24px' }} />}
+                                onClick={handleAddToCart}
+                            >
+                                Thêm vào giỏ hàng
+                            </Button>
+                        )}
+                    </div>
                 </div>
-
-                <div className={cx('product-action')}>
-                    {isInCart ? (
-                        <Button
-                            outline
-                            leftIcon={<RemoveShoppingCartOutlined color="primary" sx={{ fontSize: '24px' }} />}
-                            onClick={handleRemoveCart}
-                        >
-                            Xóa khỏi giỏ hàng
-                        </Button>
-                    ) : (
-                        <Button
-                            outline
-                            leftIcon={<AddShoppingCartOutlined color="primary" sx={{ fontSize: '24px' }} />}
-                            onClick={handleAddToCart}
-                        >
-                            Thêm vào giỏ hàng
-                        </Button>
-                    )}
+            </div>
+            <div className={cx('product-description')}>
+                <div className={cx('group')}>
+                    <div className={cx('title')}>Thông Tin Chi Tiết</div>
+                    <table className={cx('table')}>
+                        <tbody>
+                            {product?.specs?.map((spec, index) => (
+                                <tr key={index}>
+                                    <td>{spec.k}</td>
+                                    <td>{spec.v}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className={cx('group')}>
+                    <div className={cx('title')}>Mô Tả Sản Phẩm</div>
+                    <p className={cx('description')}>{product.description}</p>
                 </div>
             </div>
             <Snackbar
@@ -187,6 +209,23 @@ function ProductDetail({ product, isInCart }) {
                     {success ? message : error}
                 </Alert>
             </Snackbar>
+            <textarea
+                name=""
+                id=""
+                cols="30"
+                rows="10"
+                onChange={(e) => {
+                    setText(e.target.value);
+                }}
+            ></textarea>
+            <button
+                onClick={() => {
+                    const form = { text };
+                    console.log(form);
+                }}
+            >
+                submit
+            </button>
         </div>
     );
 }
