e42780e1d0622193abc8ddf9dda8f084afaac0f1
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-02-13 23:31:23 +0700

    add create review services

--- frontend/src/services/reviewsService.js
+++ frontend/src/services/reviewsService.js
@@ -5,6 +5,15 @@ export const getReviews = async (slug, signal) => {
         const result = await request.get(`/api/reviews/${slug}`, { signal: signal });
         return result.data;
     } catch (error) {
-        return error;
+        throw error;
+    }
+};
+
+export const createReview = async (params) => {
+    try {
+        const result = await request.get(`/api/reviews/add`, params);
+        return result.data;
+    } catch (error) {
+        throw error;
     }
 };
