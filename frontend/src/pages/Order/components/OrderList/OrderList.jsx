28043a59ac86b8a7a497c23aa237f05f83152d40
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-02-14 21:20:06 +0700

    order list

--- frontend/src/pages/Order/components/OrderList/OrderList.jsx
+++ frontend/src/pages/Order/components/OrderList/OrderList.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react';
-import { Block, CreditCard, LocalShipping, Star } from '@mui/icons-material';
+import { Block, CreditCard, LocalShipping } from '@mui/icons-material';
 import classNames from 'classnames/bind';
 import { useNavigate } from 'react-router-dom';
 
+import styles from './OrderList.module.scss';
 import assets from '@/assets';
 import Button from '@/components/Button';
 import { formatVND } from '@/helpers/number';
-import PopupCreateReview from '../PopupCreateReview/PopupCreateReview';
-import styles from './OrderList.module.scss';
-import { Rating } from '@mui/material';
+import { getOrderStatusTitle } from '@/helpers/string';
 
 const cx = classNames.bind(styles);
 
 function OrderList({ orders, onCancelOrder }) {
-    const [openPopup, setOpenPopup] = useState(false);
     const navigate = useNavigate();
 
     const getStatusIcon = (status) => {
@@ -26,24 +23,9 @@ function OrderList({ orders, onCancelOrder }) {
         return statusIcon[status] || null;
     };
 
-    const getStatusTitle = (status) => {
-        const statusTitle = {
-            awaiting_payment: 'Chờ thanh toán',
-            shipping: 'Đang vận chuyển',
-            completed: 'Đã giao',
-            canceled: 'Đã hủy',
-        };
-        return statusTitle[status] || '';
-    };
-
     const getTotalPrice = (orders) => {
         const totalPrice = orders.reduce((totalPrice, item) => totalPrice + item.productId.price * +item.quantity, 0);
         return formatVND(totalPrice);
-    };
-
-    //create review
-    const handleCreateReview = () => {
-        console.log('create review');
     };
 
     return (
@@ -52,10 +34,16 @@ function OrderList({ orders, onCancelOrder }) {
                 <div className={cx('order-item')} key={order._id}>
                     <div className={cx('order-status')}>
                         {getStatusIcon(order.status)}
-                        <span>{getStatusTitle(order.status)}</span>
+                        <span>{getOrderStatusTitle(order.status)}</span>
                     </div>
                     {order.products.map((product) => (
-                        <div className={cx('order-product')} key={product.productId._id}>
+                        <div
+                            className={cx('order-product')}
+                            key={product.productId._id}
+                            onClick={() => {
+                                navigate(`detail/${order._id}`);
+                            }}
+                        >
                             <div className={cx('product-detail')}>
                                 <div className={cx('product-image')}>
                                     <img src={product.productId.productImage} alt={product.productId.name} />
@@ -97,17 +85,6 @@ function OrderList({ orders, onCancelOrder }) {
                                     Mua lại
                                 </Button>
                             )}
-                            {order.status == 'completed' && (
-                                <Button
-                                    outline
-                                    className={cx('btn')}
-                                    onClick={() => {
-                                        setOpenPopup(true);
-                                    }}
-                                >
-                                    Viết nhận xét
-                                </Button>
-                            )}
                             <Button outline className={cx('btn')}>
                                 Xem chi tiết
                             </Button>
@@ -121,30 +98,6 @@ function OrderList({ orders, onCancelOrder }) {
                     <p>Chưa có đơn hàng</p>
                 </div>
             )}
-            <PopupCreateReview
-                openPopup={openPopup}
-                setOpenPopup={setOpenPopup}
-                title="Viết nhận xét"
-                onConfirm={handleCreateReview}
-            >
-                <div className={cx('review-form')}>
-                    <div className={cx('rating')}>
-                        <span className={cx('title')}>Vui lòng đánh giá</span>
-                        <Rating
-                            className={cx('star')}
-                            onChange={(event, newValue) => {
-                                console.log(newValue);
-                            }}
-                            size="large"
-                        />
-                    </div>
-                    <textarea
-                        cols={150}
-                        className={cx('review-field')}
-                        placeholder="Hãy chia sẻ cảm nhận, đánh giá của bạn về sản phẩm này nhé."
-                    ></textarea>
-                </div>
-            </PopupCreateReview>
         </div>
     );
 }
