9c53f9b5aacb55ff21c3060adb08bf677bf55926
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-02-17 12:06:35 +0700

    not found page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames/bind';
 
 import styles from './NotFound.module.scss';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
@@ -12,6 +13,9 @@ function NotFound(props) {
                 <p>Trang bạn đang tìm kiếm có thể đã bị di chuyển, bị xóa hoặc có thể chưa từng tồn tại.</p>
             </div>
             <span>404</span>
+            <Link to="/" className={cx('nav-link')}>
+                Trở về trang chủ
+            </Link>
         </div>
     );
 }
