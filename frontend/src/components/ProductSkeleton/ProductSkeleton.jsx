fcc3a34bfa068f3ce7b441c8f870d711ca8ed1f1
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-03-29 11:37:34 +0700

    product list skeleton

--- frontend/src/components/ProductSkeleton/ProductSkeleton.jsx
+++ frontend/src/components/ProductSkeleton/ProductSkeleton.jsx
@@ -10,7 +10,7 @@ function ProductSkeleton(props) {
     return (
         <div className={cx('product-skeleton')}>
             <Grid container spacing={2} sx={{ marginLeft: '-8px' }}>
-                {[1, 2, 3, 4, 5, 6].map((skeleton) => (
+                {[...Array(8)].map((skeleton) => (
                     <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={skeleton} sx={{ padding: '8px !important' }}>
                         <Skeleton variant="rounded" sx={{ fontSize: '2rem' }} height={300} />
                     </Grid>
