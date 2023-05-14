aba0c5a2f9773d0dcd32f35c34ce44351385087b
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-05-14 13:17:50 +0700

    fix key warning

--- frontend/src/components/ProductSkeleton/ProductSkeleton.jsx
+++ frontend/src/components/ProductSkeleton/ProductSkeleton.jsx
@@ -10,8 +10,8 @@ function ProductSkeleton(props) {
     return (
         <div className={cx('product-skeleton')}>
             <Grid container spacing={2} sx={{ marginLeft: '-8px' }}>
-                {[...Array(8)].map((skeleton) => (
-                    <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={skeleton} sx={{ padding: '8px !important' }}>
+                {[...Array(8)].map((e, index) => (
+                    <Grid item xs={12} sm={6} md={4} lg={4} xl={3} key={index} sx={{ padding: '8px !important' }}>
                         <Skeleton variant="rounded" sx={{ fontSize: '2rem' }} height={400} />
                     </Grid>
                 ))}
